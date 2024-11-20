040be20bdeeaa03d25530ef686d74fd7716519ee
Cameist <0kay0802@gmail.com>
2024-11-20 15:44:11 +0900

    style: prettier 설정

--- src/pages/record.tsx
+++ src/pages/record.tsx
@@ -1,30 +1,29 @@
 import { Pagination } from "../shared/chakra-ui/pagination";
-import { Field } from "../shared/chakra-ui/recordBox"
+import { Field } from "../shared/chakra-ui/recordBox";
 import { Stack, Heading, Button, Input } from "@chakra-ui/react";
 
 function Record() {
     return (
-        <><div>내 기록</div>
-            <Stack>                    
-                <Field
-                    label="회사이름"
-                    required>
-                    <Input/>
-                </Field>
-                <Field
-                    label="면접 유형">
-                    <Input/>
-                </Field>
-                <Field
-                    label="면접 질문">
-                    <Input/>
-                </Field>
-                <Button>등록</Button>
+        <>
+            <Stack>
+                <Heading>내 기록</Heading>
+                <Stack>
+                    <Field label="회사이름" required>
+                        <Input />
+                    </Field>
+                    <Field label="면접유형">
+                        <Input />
+                    </Field>
+                    <Field label="면접질문">
+                        <Input />
+                    </Field>
+                    <Button>등록</Button>
+                </Stack>
             </Stack>
 
-        <Pagination />
+            <Pagination />
         </>
     );
-  };
-  
-  export default Record;
+}
+
+export default Record;
