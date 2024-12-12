e3b6f8b1a2bb0b9fcfce04acd0bb9fe75991d76b
Cameist <0kay0802@gmail.com>
2024-12-12 16:46:15 +0900

    style:  Token->TOKEN  변경

--- src/pages/records/api/recordsApi.ts
+++ src/pages/records/api/recordsApi.ts
@@ -2,25 +2,25 @@ import axios from "axios";
 import { InterviewRecordCreateDTO, InterviewRecordResponseDTO, InterviewRecordUpdateDTO } from "./recordsApiList";
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
-const Token = import.meta.env.VITE_TOKEN;
+const TOKEN = import.meta.env.VITE_TOKEN;
 
 export const fetchSidebarData = async (): Promise<{ id: string; enterpriseName: string; category: string }[]> => {
-    const response = await axios.get(`${SERVER_URL}/records/sidebar`, {headers: { Authorization: `Bearer ${Token}` }});
+    const response = await axios.get(`${SERVER_URL}/records/sidebar`, {headers: { Authorization: `Bearer ${TOKEN}` }});
     return response.data;
 };
 
 export const fetchRecordDetails = async (ResponseParams: string): Promise<InterviewRecordResponseDTO> => {
-    const response = await axios.get(`${SERVER_URL}/records/interview`, {params: ResponseParams, headers:{'Authorization': `Bearer ${Token}`}});
+    const response = await axios.get(`${SERVER_URL}/records/interview`, {params: ResponseParams, headers:{'Authorization': `Bearer ${TOKEN}`}});
     return response.data;
 };
 
 export const updateRecordDetails = async (id: string, data: InterviewRecordUpdateDTO) => {
-    await axios.put(`${SERVER_URL}/records/${id}`, data, {headers:{'Authorization': `Bearer ${Token}`, "Content-Type": "application/json"}});};
+    await axios.put(`${SERVER_URL}/records/${id}`, data, {headers:{'Authorization': `Bearer ${TOKEN}`, "Content-Type": "application/json"}});};
 
 export const createRecord = async (data: InterviewRecordCreateDTO) => {
-    await axios.post(`${SERVER_URL}/records/interview`, data, {headers:{'Authorization': `Bearer ${Token}`, "Content-Type": "application/json"}});};
+    await axios.post(`${SERVER_URL}/records/interview`, data, {headers:{'Authorization': `Bearer ${TOKEN}`, "Content-Type": "application/json"}});};
 
 export const fetchRecords = async (ResponseParams: string): Promise<Record<string, InterviewRecordResponseDTO>> => {
-    const response = await axios.get(`${SERVER_URL}/records/interview`, {params: ResponseParams , headers:{'Authorization': `Bearer ${Token}`}});    
+    const response = await axios.get(`${SERVER_URL}/records/interview`, {params: ResponseParams , headers:{'Authorization': `Bearer ${TOKEN}`}});    
     return response.data;
 };
