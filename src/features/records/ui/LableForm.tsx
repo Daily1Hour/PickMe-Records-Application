85f2626a7c652141e68ecab18659a07a6cc3b262
Cameist <0kay0802@gmail.com>
2025-01-14 19:54:08 +0900

    refactor: controller 구조 변경

--- src/features/records/ui/LableForm.tsx
+++ src/features/records/ui/LableForm.tsx
@@ -1,33 +1,30 @@
-import { Controller, useFormContext } from "react-hook-form"
-import { Stack, Input } from "@chakra-ui/react"
+import { Controller, useFormContext } from "react-hook-form";
+import { Stack, Input } from "@chakra-ui/react";
 
 export const LabelForm = () => {
     const { control } = useFormContext();
 
+    const types = ["enterpriseName", "category"];
+    const korType: Record<string, string> = {
+        enterpriseName: "회사 이름",
+        category: "면접 유형",
+    };
+
     return (
         <Stack gap="10">
-            <Controller
-                name="enterpriseName"
-                control={control}
-                render={({ field }) => (
-                    <Input
-                        {...field}
-                        variant="flushed"
-                        placeholder="회사 이름"
-                    />
-                )}
-            />
-            <Controller
-                name="category"
-                control={control}
-                render={({ field }) => (
-                    <Input
-                        {...field}
-                        variant="flushed"
-                        placeholder="면접 유형"
-                    />
-                )}
-            />
+            {types.map((type) => (
+                <Controller
+                    name={type}
+                    control={control}
+                    render={({ field }) => (
+                        <Input
+                            {...field}
+                            variant="flushed"
+                            placeholder={`${korType[type]}`}
+                        />
+                    )}
+                />
+            ))}
         </Stack>
     );
-}
+};
