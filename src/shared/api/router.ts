bbcb8a262c946d57682100e88a0ad0c7d602e590
NarciSource <jwch11@gmail.com>
2025-02-02 20:07:48 +0900

    refactor: 라우터 의존성역전 (함수형)

--- src/shared/api/router.ts
+++ src/shared/api/router.ts
@@ -1,28 +1,20 @@
 import type { Router } from "@remix-run/router";
 
 // 의존성역전 사용 (DIP)
-class RouteHandler {
-    private router?: Router;
+let router: Router | undefined;
 
-    // 의존성 주입
-    public setRouter(router: Router) {
-        this.router = router;
+// 의존성 주입
+const setRouter = (newRouter: Router) => {
+    router = newRouter;
+};
+
+// 간접 네비게이션 수행
+const navigateTo = (path: string) => {
+    if (router) {
+        router.navigate(path);
+    } else {
+        throw new Error("Router is not set");
     }
-
-    // 간접 네비게이션 수행
-    public navigateTo(path: string) {
-        if (this.router) {
-            this.router.navigate(path);
-        } else {
-            throw new Error("Router is not set");
-        }
-    }
-}
-
-// 싱글톤 인스턴스 생성
-const navigator = new RouteHandler();
-// 컨텍스트 고정 후 멤버함수 추출
-const navigateTo = navigator.navigateTo.bind(navigator);
-const setRouter = navigator.setRouter.bind(navigateTo);
+};
 
 export { setRouter, navigateTo };
