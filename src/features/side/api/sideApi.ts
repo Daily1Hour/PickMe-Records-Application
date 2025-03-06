034b159828a99e2eb0144e963011d65cc8b4072a
NarciSource <jwch11@gmail.com>
2025-03-06 21:50:21 +0900

    refactor: axios 클라이언트 별도의 모듈로 분리

--- src/features/side/api/sideApi.ts
+++ src/features/side/api/sideApi.ts
@@ -1,21 +1,8 @@
-import axios from "axios";
-
-import { accessToken } from "@/shared/api/tokens";
+import { client } from "@/shared/api";
 import { Summary } from "@/entities/records/model/Summary";
 import { dtoToSide } from "../service/dtoToSide";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL;
-
-
-const client = axios.create({
-    baseURL: `${SERVER_URL}`,
-    headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json"
-    }
-});
-
 export const fetchSidebarData = async (): Promise<Summary[]> => {
-    const response = await client.get('/sidebar');
+    const response = await client.get("/sidebar");
     return dtoToSide(response.data);
 };
