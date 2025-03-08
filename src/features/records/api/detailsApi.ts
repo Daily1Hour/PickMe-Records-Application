4a8d00ebe03c66132e87ba8ef4d6d2e0e426d60b
NarciSource <jwch11@gmail.com>
2025-03-08 21:40:44 +0900

    refactor: API 경로 수정

--- src/features/records/api/detailsApi.ts
+++ src/features/records/api/detailsApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { accessToken } from "@/shared/api/token";
+import { accessToken } from "@/shared/api/tokens";
 import {
     InterviewRecordResponseDTO,
     RecordDetailCreateDTO,
@@ -15,7 +15,7 @@ import { RecordType } from "../model/RecordSchema";
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 const client = axios.create({
-    baseURL: `${SERVER_URL}/record`,
+    baseURL: `${SERVER_URL}`,
     headers: {
         Authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/json",
--- src/features/side/api/sideApi.ts
+++ src/features/side/api/sideApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { accessToken } from "@/shared/api/token";
+import { accessToken } from "@/shared/api/tokens";
 import { Summary } from "@/entities/records/model/Summary";
 import { dtoToSide } from "../service/dtoToSide";
 
@@ -8,7 +8,7 @@ const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 
 const client = axios.create({
-    baseURL: `${SERVER_URL}/record`,
+    baseURL: `${SERVER_URL}`,
     headers: {
         Authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/json"
